4f9815bf4ffc64c375b3a4e4a892fde8402602b3
sbx_user1051 <sbx_user1051@169.254.6.77>
2025-04-15 16:02:27 +0000

    Tue Apr 15 16:02:27 UTC 2025

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -7,19 +7,25 @@ class CheckoutSolution:
         # IDEA: have a cart
         cart = [[], [], []]  # Freebies, Discounts, everything else
 
-        for sku in skus:
-            if sku not in allowed:
-                return -1
-            count[sku] = count.get(sku, 0) + 1
-
         prices = {"A": 50, "B": 30, "C": 20, "D": 15, "E": 40, "F": 10, "G": 20, "H": 10, "I": 35,
                   "J": 60, "K": 80, "L": 90, "M": 15, "N": 40, "O": 10, "P": 50, "Q": 30, "R": 50, "S": 30, "T": 20, "U": 40, "V": 50, "W": 20, "X": 90, "Y": 10, "Z": 50}
         # SKU -> [[num, price], [num, price]]
         special_discount = {"A": [[3, 150], [5, 200]], "B": [[2, 45]], "H": [
             [5, 45], [10, 80]], "K": [[2, 150]], "P": [[5, 200]], "Q": [[3, 80]], "V": [[2, 90], [3, 130]]}
         # SKU -> [[num, SKU]]
-        specials_freebie = {"E": [2, "B"], "F": [2, "F"], "N": [
+        special_freebie = {"E": [2, "B"], "F": [2, "F"], "N": [
             3, "M"], "R": [3, "Q"], "U": [3, "U"]}
+
+        for sku in skus:
+            if sku not in allowed:
+                return -1
+            count[sku] = count.get(sku, 0) + 1
+            if sku in special_freebie:
+                cart[0].append(sku)
+            elif sku in special_discount:
+                cart[1].append(sku)
+            else:
+                cart[2].append(sku)
 
         def process(sku):
 
@@ -100,6 +106,7 @@ class CheckoutSolution:
 
         # can reduce amount of B instead in lookup
         return total
+
 
 
 
