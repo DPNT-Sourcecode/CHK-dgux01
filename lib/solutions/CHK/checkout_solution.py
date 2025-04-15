c63d683ee485b10393bdca24e5c23d7c8592e778
sbx_user1051 <sbx_user1051@169.254.10.73>
2025-04-15 12:03:36 +0000

    Tue Apr 15 12:03:36 UTC 2025

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -3,4 +3,10 @@ class CheckoutSolution:
 
     # skus = unicode string
     def checkout(self, skus):
-        raise NotImplementedError()
+        # assume string is just string of ABCs
+        # Need to keep track of item: price
+        # need to find out how many, maybe lookup in dictionary
+        # Need to apply special discount, maybe % and //
+        prices = {"A": 50, "B": 30, "C": 20, "D": 15}
+        count = dict()
+
