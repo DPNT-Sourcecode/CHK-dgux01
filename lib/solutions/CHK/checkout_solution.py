ab2a6466ac60330b88f5b0e4c3670efb40aed696
sbx_user1051 <sbx_user1051@169.254.10.73>
2025-04-15 13:23:58 +0000

    Tue Apr 15 13:23:58 UTC 2025

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -5,31 +5,32 @@ class CheckoutSolution:
     # If more products, might need to make a super process, then do case match
     def checkout(self, skus):
         def process_A(self, num_of_a):
-        multiples_5 = num_of_a // 5
-        remainder_of_5 = num_of_a % 5
-        multiples_3 = remainder_of_5 // 3
-        remainder_of_3 = remainder_of_5 % 3
+            multiples_5 = num_of_a // 5
+            remainder_of_5 = num_of_a % 5
+            multiples_3 = remainder_of_5 // 3
+            remainder_of_3 = remainder_of_5 % 3
 
-        return (multiples_5 * 200) + (multiples_3 * 130) + (remainder_of_3 * 50)
+            return (multiples_5 * 200) + (multiples_3 * 130) + (remainder_of_3 * 50)
 
-    def process_B(self, num_of_b):
-        # What if number of B is less than 0
-        if num_of_b < 1:
-            return 0
-        multiples_2 = num_of_b // 2
-        remainder_of_2 = num_of_b % 2
+        def process_B(self, num_of_b):
+            # What if number of B is less than 0
+            if num_of_b < 1:
+                return 0
+            multiples_2 = num_of_b // 2
+            remainder_of_2 = num_of_b % 2
 
-        return (multiples_2 * 45) + (remainder_of_2 * 30)
+            return (multiples_2 * 45) + (remainder_of_2 * 30)
 
-    def process_E(self, num_of_e):
-        num_free_b = num_of_e // 2
-        return num_free_b, num_of_e * 40
+        def process_E(self, num_of_e):
+            num_free_b = num_of_e // 2
+            return num_free_b, num_of_e * 40
 
-    def process_C(self, num_of_c):
-        return num_of_c * 20
+        def process_C(self, num_of_c):
+            return num_of_c * 20
 
-    def process_D(self, num_of_d):
-        return num_of_d * 15
+        def process_D(self, num_of_d):
+            return num_of_d * 15
+
         allowed = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
         total = 0
         if not skus:
@@ -47,25 +48,21 @@ class CheckoutSolution:
                 continue
             match p:
                 case "E":
-                    free_b, amount_e = self.process_E(count.get("E"))
+                    free_b, amount_e = process_E(count.get("E"))
                     count["B"] -= free_b
                     total += amount_e
                 case "A":
-                    amount_a = self.process_A(count.get("A"))
+                    amount_a = process_A(count.get("A"))
                     total += amount_a
                 case "B":
-                    amount_b = self.process_B(count.get("B"))
+                    amount_b = process_B(count.get("B"))
                     total += amount_b
                 case "C":
-                    amount_c = self.process_C(count.get("C"))
+                    amount_c = process_C(count.get("C"))
                     total += amount_c
                 case "D":
-                    amount_d = self.process_D(count.get("D"))
+                    amount_d = process_D(count.get("D"))
                     total += amount_d
 
         # can reduce amount of B instead in lookup
         return total
-
-
-
-
