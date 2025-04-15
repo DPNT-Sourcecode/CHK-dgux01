4a6b2c81bd7746392d36c5cb292ab552cc05d6d4
sbx_user1051 <sbx_user1051@169.254.10.73>
2025-04-15 13:23:17 +0000

    Tue Apr 15 13:23:17 UTC 2025

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -4,6 +4,32 @@ class CheckoutSolution:
     # skus = unicode string
     # If more products, might need to make a super process, then do case match
     def checkout(self, skus):
+        def process_A(self, num_of_a):
+        multiples_5 = num_of_a // 5
+        remainder_of_5 = num_of_a % 5
+        multiples_3 = remainder_of_5 // 3
+        remainder_of_3 = remainder_of_5 % 3
+
+        return (multiples_5 * 200) + (multiples_3 * 130) + (remainder_of_3 * 50)
+
+    def process_B(self, num_of_b):
+        # What if number of B is less than 0
+        if num_of_b < 1:
+            return 0
+        multiples_2 = num_of_b // 2
+        remainder_of_2 = num_of_b % 2
+
+        return (multiples_2 * 45) + (remainder_of_2 * 30)
+
+    def process_E(self, num_of_e):
+        num_free_b = num_of_e // 2
+        return num_free_b, num_of_e * 40
+
+    def process_C(self, num_of_c):
+        return num_of_c * 20
+
+    def process_D(self, num_of_d):
+        return num_of_d * 15
         allowed = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
         total = 0
         if not skus:
@@ -39,34 +65,7 @@ class CheckoutSolution:
 
         # can reduce amount of B instead in lookup
         return total
-    # Probably need to process E's first, to determine number of B's
 
-    def process_A(self, num_of_a):
-        multiples_5 = num_of_a // 5
-        remainder_of_5 = num_of_a % 5
-        multiples_3 = remainder_of_5 // 3
-        remainder_of_3 = remainder_of_5 % 3
-
-        return (multiples_5 * 200) + (multiples_3 * 130) + (remainder_of_3 * 50)
-
-    def process_B(self, num_of_b):
-        # What if number of B is less than 0
-        if num_of_b < 1:
-            return 0
-        multiples_2 = num_of_b // 2
-        remainder_of_2 = num_of_b % 2
-
-        return (multiples_2 * 45) + (remainder_of_2 * 30)
-
-    def process_E(self, num_of_e):
-        num_free_b = num_of_e // 2
-        return num_free_b, num_of_e * 40
-
-    def process_C(self, num_of_c):
-        return num_of_c * 20
-
-    def process_D(self, num_of_d):
-        return num_of_d * 15
 
 
 
