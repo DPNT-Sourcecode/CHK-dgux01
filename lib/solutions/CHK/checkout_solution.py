67cde443c6a1da01e948a552098cc46eafbc094d
sbx_user1051 <sbx_user1051@169.254.6.77>
2025-04-15 15:50:27 +0000

    Tue Apr 15 15:50:27 UTC 2025

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -8,10 +8,12 @@ class CheckoutSolution:
     def checkout(self, skus):
         prices = {"A": 50, "B": 30, "C": 20, "D": 15, "E": 40, "F": 10, "G": 20, "H": 10, "I": 35,
                   "J": 60, "K": 80, "L": 90, "M": 15, "N": 40, "O": 10, "P": 50, "Q": 30, "R": 50, "S": 30, "T": 20, "U": 40, "V": 50, "W": 20, "X": 90, "Y": 10, "Z": 50}
-        specials_discount = dict()  # SKU -> [[num, price], [num, price]]
+        # SKU -> [[num, price], [num, price]]
         special_discount = {"A": [[3, 150], [5, 200]], "B": [[2, 45]], "H": [
-            [5, 45], [10, 80]], "K": [[2, 150]], "P": [[5, 200]], "Q": [[3, 80]]}
-        specials_freebie = dict()  # SKU -> [[num, SKU]]
+            [5, 45], [10, 80]], "K": [[2, 150]], "P": [[5, 200]], "Q": [[3, 80]], "V": [[2, 90], [3, 130]]}
+
+        specials_freebie = {"E": [2, "B"], "F": [2, "F"], "N": [
+            3, "M"], "R": [3, "Q"]}  # SKU -> [[num, SKU]]
 
         def process(sku):
             pass
@@ -92,3 +94,4 @@ class CheckoutSolution:
 
         # can reduce amount of B instead in lookup
         return total
+
